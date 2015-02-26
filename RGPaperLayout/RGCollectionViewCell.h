0aee56036f4c7c6822eb4151edcf623b71a0dd14
Robera Geleta <robera.geleta@vensi.com>
2015-02-25 21:28:08 -0600

    added custom collection view cell

---
+++ RGPaperLayout/RGCollectionViewCell.h
@@ -0,0 +1,16 @@
+//
+//  RGCollectionViewCell.h
+//  RGPaperLayout
+//
+//  Created by ROBERA GELETA on 2/25/15.
+//  Copyright (c) 2015 ROBERA GELETA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RGCollectionViewCell : UICollectionViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UILabel *label;
+
+
+@end
---
+++ RGPaperLayout/RGCollectionViewCell.m
@@ -0,0 +1,16 @@
+//
+//  RGCollectionViewCell.m
+//  RGPaperLayout
+//
+//  Created by ROBERA GELETA on 2/25/15.
+//  Copyright (c) 2015 ROBERA GELETA. All rights reserved.
+//
+
+#import "RGCollectionViewCell.h"
+
+@implementation RGCollectionViewCell
+- (void)awakeFromNib
+{
+    self.imageView.contentMode = UIViewContentModeScaleAspectFill;
+}
+@end
